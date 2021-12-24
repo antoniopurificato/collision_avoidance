8dd8cb7cb7b2a70908f2c055b17006b2ed4822b9
antoniopurificato <purificato.219135@studenti.uniroma1.it>
2021-12-24 18:29:10 +0100

    Subscriber for the cmd_vel and first attempts for the subscriber of the laser_scan. Actually does not work

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Repository of the code for the project of Robot Programming 2021/22.<br/>
 Collision avoidance project.
 
 ### TODO:
-- [ ] ALL
+- [x] Create a ros nonde
+- [ ] Complete the subscriber for the velocity
--- collision_avoidance/src/collision_avoider.cpp
+++ collision_avoidance/src/collision_avoider.cpp
@@ -12,13 +12,33 @@ ros::Publisher velocity;
 bool command_received = false;
 float vel_x, vel_y, vel_angular = 0;
 
-void cmd_vel_Callback(const geometry_msgs::Twist::ConstPtr& msg){
+void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg){
+    
     command_received=true; 
     vel_x=msg->linear.x;
     vel_y=msg->linear.y; 
-    vel_angular=msg->vel_angular.z;
-
+    vel_angular=msg->angular.z;
 }
 
-int main(int argc, char **argv)
-{}
+void laser_callback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+ if (!cmdReceived ) return;
+ cmdReceived = false;
+}
+
+int main(int argc, char **argv){
+
+    /*Starting the collision avoidance node */
+    ros::init(argc, argv, "collision_avoidance"); 
+    ros::NodeHandle n;
+
+    /*Subscriber for the velocity command (topic) of the robot*/
+    ros::Subscriber cmd_vel_sub = n.subscribe("cmd_vel", 1, cmd_vel_callback);
+
+    /*Subscriber for the laser scan (topic) of the robot*/
+    ros::Subscriber laser_scan_sub = n.subscribe("base_scan", 1, laser_callback);
+    ROS_INFO("Collision avoidance node is active!\n");
+    velocity = n.advertise<geometry_msgs::Twist>("cmd_vel",1000);
+    ros::spin();
+    return 0;
+}
